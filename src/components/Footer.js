22fa2c961b0d22d50931171c1e8e8ca93f44fd06
ivanaimtechai <tumacayivan@gmail.com>
2026-02-15 00:17:44 +0800

    new update in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,9 @@ function Footer() {
   return (
     <footer className="footer">
       <p>IvanSwarmAI © {new Date().getFullYear()}</p>
+      <p style={{ marginTop: '0.5rem' }}>
+        Researcher/Developer: <a href="mailto:tumacayivan@gmail.com">Ivan Tumacay</a>
+      </p>
     </footer>
   );
 }
